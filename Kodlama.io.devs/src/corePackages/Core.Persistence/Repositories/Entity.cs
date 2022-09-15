1baf9aaa74020160a785fd513a5d4ca499e9cc86
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-15 20:38:38 +0300

    Commit 6 | Core Persistence Files Created

---
+++ Kodlama.io.devs/src/corePackages/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity
+{
+    public int Id { get; set; }
+    public DateTime? CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+
+    public Entity()
+    {
+    }
+
+    public Entity(int id) : this()
+    {
+        Id = id;
+        CreateDate = DateTime.Now.Date;
+    }
+}
