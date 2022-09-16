ef6c5702c9c21f6b974122a201d9175000645d3c
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-16 20:15:55 +0300

    Commit 10 | [task#1] Language Entity Created

---
+++ Kodlama.io.devs/src/projects/Kodlama.Io.Devs/Kodlama.Io.Devs.Domain/Entities/Language.cs
@@ -0,0 +1,20 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Kodlama.Io.Devs.Domain.Entities
+{
+    public class Language : Entity
+    {
+        public string Name { get; set; }
+
+        public Language()
+        {
+        }
+
+        public Language(int id, string name):this()
+        {
+            Id = id;
+            Name = name;
+            CreateDate = DateTime.Now;
+        }
+    }
+}
