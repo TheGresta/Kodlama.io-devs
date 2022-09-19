346dbd64beb6d4f5d134dbf9dc62f481853be73e
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-19 21:27:43 +0300

    Commit 36 | [task#2] Language Technology + GitHub Mapping Profiles Updated

--- Kodlama.io.devs/src/projects/Kodlama.Io.Devs/Kodlama.Io.Devs.Application/Features/GitHubs/Profiles/MappingProfiles.cs
+++ Kodlama.io.devs/src/projects/Kodlama.Io.Devs/Kodlama.Io.Devs.Application/Features/GitHubs/Profiles/MappingProfiles.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Core.Persistence.Paging;
+using Kodlama.Io.Devs.Application.Features.GitHubs.Commands.CreateGitHub;
+using Kodlama.Io.Devs.Application.Features.GitHubs.Commands.UpdateGitHub;
 using Kodlama.Io.Devs.Application.Features.GitHubs.Dtos;
 using Kodlama.Io.Devs.Application.Features.GitHubs.Models;
 using Kodlama.Io.Devs.Domain.Entities;
@@ -10,7 +12,7 @@ namespace Kodlama.Io.Devs.Application.Features.GitHubs.Profiles
     {
         public MappingProfiles()
         {
-            CreateMap<GitHub, UpdatedGitHubDto>().ReverseMap();
+            CreateMap<GitHub, CreateGitHubCommand>().ReverseMap();
             CreateMap<GitHub, CreatedGitHubDto>()
                 .ForMember(g => g.UserName, opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
                 .ForMember(g => g.UserMail, opt => opt.MapFrom(src => $"{src.User.Email}"))
@@ -21,7 +23,7 @@ namespace Kodlama.Io.Devs.Application.Features.GitHubs.Profiles
                 .ForMember(g => g.UserMail, opt => opt.MapFrom(src => $"{src.User.Email}"))
                 .IncludeMembers(g => g.User).ReverseMap();
 
-            CreateMap<GitHub, UpdatedGitHubDto>().ReverseMap();
+            CreateMap<GitHub, UpdateGitHubCommand>().ReverseMap();
             CreateMap<GitHub, UpdatedGitHubDto>()
                 .ForMember(g => g.UserName, opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
                 .ForMember(g => g.UserMail, opt => opt.MapFrom(src => $"{src.User.Email}"))
--- Kodlama.io.devs/src/projects/Kodlama.Io.Devs/Kodlama.Io.Devs.Application/Features/LanguageTechnologies/Profiles/MappingProfiles.cs
+++ Kodlama.io.devs/src/projects/Kodlama.Io.Devs/Kodlama.Io.Devs.Application/Features/LanguageTechnologies/Profiles/MappingProfiles.cs
@@ -1,4 +1,10 @@
 ﻿using AutoMapper;
+using Core.Persistence.Paging;
+using Kodlama.Io.Devs.Application.Features.LanguageTechnologies.Commands.CreateLanguageTechnology;
+using Kodlama.Io.Devs.Application.Features.LanguageTechnologies.Commands.UpdateLanguageTechnology;
+using Kodlama.Io.Devs.Application.Features.LanguageTechnologies.Dtos;
+using Kodlama.Io.Devs.Application.Features.LanguageTechnologies.Models;
+using Kodlama.Io.Devs.Domain.Entities;
 
 namespace Kodlama.Io.Devs.Application.Features.LanguageTechnologies.Profiles
 {
@@ -6,7 +12,28 @@ namespace Kodlama.Io.Devs.Application.Features.LanguageTechnologies.Profiles
     {
         public MappingProfiles()
         {
+            CreateMap<LanguageTechnology, CreateLanguageTechnologyCommand>().ReverseMap();
+            CreateMap<LanguageTechnology, CreatedLanguageTechnologyDto>()
+                .ForMember(l => l.LanguageName, opt => opt.MapFrom(src => $"{src.Language.Name}"))
+                .IncludeMembers(l => l.Language).ReverseMap();
 
+            CreateMap<LanguageTechnology, DeletedLanguageTechnologyDto>()
+                .ForMember(l => l.LanguageName, opt => opt.MapFrom(src => $"{src.Language.Name}"))
+                .IncludeMembers(l => l.Language).ReverseMap();
+
+            CreateMap<LanguageTechnology, UpdateLanguageTechnologyCommand>().ReverseMap();
+            CreateMap<LanguageTechnology, UpdatedLanguageTechnologyDto>()
+                .ForMember(l => l.LanguageName, opt => opt.MapFrom(src => $"{src.Language.Name}"))
+                .IncludeMembers(l => l.Language).ReverseMap();
+
+            CreateMap<LanguageTechnology, GetByIdLanguageTechnologyDto>()
+                .ForMember(l => l.LanguageName, opt => opt.MapFrom(src => $"{src.Language.Name}"))
+                .IncludeMembers(l => l.Language).ReverseMap();
+
+            CreateMap<IPaginate<LanguageTechnology>, LanguageTechnologyListModel>().ReverseMap();
+            CreateMap<LanguageTechnology, ListLanguageTechnologyDto>()
+                .ForMember(l => l.LanguageName, opt => opt.MapFrom(src => $"{src.Language.Name}"))
+                .IncludeMembers(l => l.Language).ReverseMap();
         }
     }
 }
