4b6aeb26529fa5389a735cfed436db99e788a81c
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-10-04 13:37:09 +0300

    Commit 71 | [improvement#2] UserAuthService Created

---
+++ Kodlama.io.devs/src/projects/Kodlama.Io.Devs/Kodlama.Io.Devs.Application/Services/UserAuthService/IUserAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Security.Entities;
+using Core.Security.JWT;
+
+namespace Kodlama.Io.Devs.Application.Services.UserAuthService
+{
+    public interface IUserAuthService
+    {
+        public Task<AccessToken> CreateAccessToken(User user);
+        public Task<RefreshToken> CreateRefreshToken(User user, string ipAddress);
+        public Task<RefreshToken> AddRefreshToken(RefreshToken refreshToken);
+        public Task AddUserOperationClaimsForCreatedUser(int userId, string[] roleNames);
+    }
+}
---
+++ Kodlama.io.devs/src/projects/Kodlama.Io.Devs/Kodlama.Io.Devs.Application/Services/UserAuthService/UserAuthService.cs
@@ -0,0 +1,66 @@
+﻿using Core.Persistence.Paging;
+using Core.Security.Entities;
+using Core.Security.JWT;
+using Kodlama.Io.Devs.Application.Features.UserOperationClaims.Commands.CreateMultipleUserOperationClaim;
+using Kodlama.Io.Devs.Application.Services.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kodlama.Io.Devs.Application.Services.UserAuthService
+{
+    public class UserAuthService : IUserAuthService
+    {
+        private readonly IUserOperationClaimRepository _userOperationClaimRepository;
+        private readonly ITokenHelper _tokenHelper;
+        private readonly IRefreshTokenRepository _refreshTokenRepository;
+        private readonly IMediator _mediator;
+
+        public UserAuthService(IUserOperationClaimRepository userOperationClaimRepository, 
+                               ITokenHelper tokenHelper, 
+                               IRefreshTokenRepository refreshTokenRepository, 
+                               IMediator mediator)
+        {
+            _userOperationClaimRepository = userOperationClaimRepository;
+            _tokenHelper = tokenHelper;
+            _refreshTokenRepository = refreshTokenRepository;
+            _mediator = mediator;
+        }
+
+        public async Task<RefreshToken> AddRefreshToken(RefreshToken refreshToken)
+        {
+            RefreshToken addedRefreshToken = await _refreshTokenRepository.AddAsync(refreshToken);
+            return addedRefreshToken;
+        }
+
+        public async Task AddUserOperationClaimsForCreatedUser(int userId, string[] roleNames)
+        {
+            CreateMultipleUserOperationClaimCommand createMultipleUserOperationClaimCommand = new()
+            {
+                UserId = userId,
+                RoleNames = roleNames
+            };
+
+            await _mediator.Send(createMultipleUserOperationClaimCommand);
+        }
+
+        public async Task<AccessToken> CreateAccessToken(User user)
+        {
+            ​IPaginate<UserOperationClaim> userOperationClaims = await _userOperationClaimRepository
+                                                                        .GetListAsync(u => u.UserId == user.Id, include:
+                                                                                      u => u.Include(u => u.OperationClaim));
+
+            IList<OperationClaim> operationClaims =
+            userOperationClaims.Items.Select(u => new OperationClaim
+            { Id = u.OperationClaim.Id, Name = u.OperationClaim.Name }).ToList();
+
+            AccessToken accessToken = _tokenHelper.CreateToken(user, operationClaims);
+            return accessToken;
+        }
+
+        public async Task<RefreshToken> CreateRefreshToken(User user, string ipAddress)
+        {
+            RefreshToken refreshToken = _tokenHelper.CreateRefreshToken(user, ipAddress);
+            return await Task.FromResult(refreshToken);
+        }
+    }
+}
